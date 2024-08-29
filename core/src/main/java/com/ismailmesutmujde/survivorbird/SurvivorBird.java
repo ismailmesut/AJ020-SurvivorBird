a0fee2f05b3110308873fcabda05b0b6cde21393
ismailmesut <ismailmesutmujde@gmail.com>
2024-08-29 14:09:01 +0300

    updated: draw background

--- core/src/main/java/com/ismailmesutmujde/survivorbird/SurvivorBird.java
+++ core/src/main/java/com/ismailmesutmujde/survivorbird/SurvivorBird.java
@@ -1,21 +1,29 @@
 package com.ismailmesutmujde.survivorbird;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class SurvivorBird extends ApplicationAdapter {
 
+    SpriteBatch batch;
+    Texture background;
 
     @Override
     public void create () {
-
+        batch = new SpriteBatch();
+        background = new Texture("background.png");
 
     }
 
     @Override
     public void render () {
-
+        batch.begin();
+        batch.draw(background, 0,0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
     }
 
     @Override
